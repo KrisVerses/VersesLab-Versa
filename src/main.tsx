3c8acdceffc492ee8e5e63d3d7c9696d09b05c85
Kris Norful <krisnorful@gmail.com>
2025-02-13 09:41:54 -0600

    chore: clean up main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Route, RouterProvider } from "react-router-dom";
-import router from "./app/routes/routes.tsx";
-import "../src/styles/index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/routes/routes.tsx";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </React.StrictMode>
 );
