8f48a15b87c60d0dcf9305634320587549559fd9
Kris Norful <krisnorful@gmail.com>
2025-02-13 09:42:28 -0600

    refactor: update routing for pages

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -3,20 +3,21 @@ import {
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
-import Home from "../../pages/Home";
-import Tasks from "../../pages/Tasks";
-import Appointments from "../../pages/Appointments";
-import Notes from "../../pages/Notes";
+import { Home } from "../../pages/Home";
+import { Tasks } from "../../pages/Tasks";
+import { Appointments } from "../../pages/Appointments";
+import { Notes } from "../../pages/Notes";
+import { Layout } from "../../components/Layout";
+import { Summary } from "../../components/Summary";
 
-const router = createBrowserRouter(
+// Ensure your router is properly typed
+export const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
-      <Route path="/" element={<Home />} />
-      <Route path="/tasks" element={<Tasks />} />
-      <Route path="/appointments" element={<Appointments />} />
-      <Route path="/notes" element={<Notes />} />
-    </>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="tasks" element={<Tasks />} />
+      <Route path="appointments" element={<Appointments />} />
+      <Route path="notes" element={<Notes />} />
+    </Route>
   )
 );
-
-export default router;
