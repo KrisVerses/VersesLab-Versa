45f9fd257083e635279171c0b00c8f9c45e28771
Kris Norful <krisnorful@gmail.com>
2025-02-13 09:40:42 -0600

    chore: update packages to correctly render tailwind styles

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: "#1E40AF",
+        customPink: "#EC4899",
+      },
+    },
+  },
+  plugins: [],
+};
