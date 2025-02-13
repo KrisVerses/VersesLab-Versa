7fd036866485c8c56417304fe00dd93f1a74a34c
Kris Norful <krisnorful@gmail.com>
2025-02-13 17:15:15 -0600

    refactor: setup to dynamically render user added notes

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,3 +1,27 @@
-export function Notes() {
-  return <h1>This is the Notes page</h1>;
-}
+import React, { useContext, useState } from "react";
+import { StateContext } from "../app/StateProvider";
+import { Note } from "../types/types";
+
+type NotesProps = {
+  notes: Note[];
+};
+
+export const Notes: React.FC = () => {
+  const { allNotes, setAllNotes } = useContext(StateContext);
+  return (
+    <div>
+      <h1>All Notes</h1>
+      {allNotes.map((note: Note) => (
+        <div>
+          <h2 className="text-2xl font-bold">{note.title}</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit
+            voluptate delectus consequatur accusantium dolores culpa vel quod
+            quaerat modi. Fugiat, nobis beatae. Libero non assumenda quaerat ab
+            totam possimus quod.
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
