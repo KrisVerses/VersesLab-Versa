4d13ba47b931a58580675ac1b82f3adf19e85958
Kris Norful <krisnorful@gmail.com>
2025-02-13 14:46:22 -0600

    refactor: provide state at top level component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes/routes.tsx";
 import "./styles/index.css";
+import { StateProvider } from "./app/StateProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <StateProvider>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </StateProvider>
 );
