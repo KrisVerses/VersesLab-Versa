4aea362e7310b44786b1b393157b2269acbb82be
KrisVerses <krisincode@gmail.com>
2025-02-19 11:39:25 -0600

    refactor: create uniformity with other Summary components

--- src/components/UpcomingAppointment.tsx
+++ src/components/UpcomingAppointment.tsx
@@ -6,17 +6,23 @@ export const UpcomingAppointment: React.FC = () => {
   const { nextAppointment } = useContext(StateContext);
   const isEmpty = !nextAppointment;
 
-  return isEmpty ? (
-    <p className="text-gray-500 italic">No upcoming appointments.</p>
-  ) : (
-    <div className="border-r-2">
+  return (
+    <div>
       <h3 className="text-lg font-bold">Next Appointment</h3>
-      <p>Title: {nextAppointment.description}</p>
-      <div className="flex items-center">
-        <p>Date: {nextAppointment.dueDate}</p>
-        <p className="mx-2"> | </p>
-        <p>Time: {convertMilitaryToRegular(nextAppointment.time)}</p>
-      </div>
+      {isEmpty ? (
+        <p className="border-r-2 text-gray-500 italic">
+          No upcoming appointments.
+        </p>
+      ) : (
+        <div className="border-r-2">
+          <p>Title: {nextAppointment.content}</p>
+          <div className="flex items-center">
+            <p>Date: {nextAppointment.dueDate}</p>
+            <p className="mx-2"> | </p>
+            <p>Time: {convertMilitaryToRegular(nextAppointment.time)}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
