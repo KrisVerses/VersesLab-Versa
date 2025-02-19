9df300b9773840804bb565799069cdbea085070b
KrisVerses <krisincode@gmail.com>
2025-02-19 11:40:25 -0600

    chore: add a helper function to trim the look of notes in notes list

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -14,3 +14,18 @@ export const convertMilitaryToRegular = (time: string): string => {
   const formattedHours = hours % 12 || 12; // Convert 0 or 12 to 12, and 13-23 to 1-11
   return `${formattedHours}:${minutes.toString().padStart(2, "0")} ${period}`;
 };
+
+export function getSnippet(text: string, maxLength: number = 60): string {
+  if (text.length <= maxLength) return text; // If the text is short enough, return it as is.
+
+  // Find the last space within the maxLength limit
+  let trimmedText = text.substring(0, maxLength);
+  let lastSpaceIndex = trimmedText.lastIndexOf(" ");
+
+  // Ensure we don't cut in the middle of a word
+  if (lastSpaceIndex > 0) {
+    trimmedText = trimmedText.substring(0, lastSpaceIndex);
+  }
+
+  return trimmedText + "...";
+}
